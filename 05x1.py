5c892f345b0db26b6bed5d8b9624034bba392883
pf226513 <pf226513@gsk.com>
2017-12-05 08:54:59 +0100

    Day 5 completed

---
+++ 05x1.py
@@ -0,0 +1,18 @@
+_input = ""
+with open("05_input.txt", "r") as f:
+    _input = f.read().split("\n")
+
+_list = [int(n) for n in _input]
+
+index = 0
+counter = 0
+while True:
+    try:
+        offset = _list[index]
+        _list[index] += 1
+        index += offset
+        counter += 1
+    except IndexError as ex:
+        break
+
+print(counter)
